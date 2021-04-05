7436e2eb9cf9064bee2fd533af81076ccac0deb2
Frederic-Guillon <frdric.guillon@gmail.com>
2021-04-05 15:47:07 +0200

    puissance 4 codé

--- js/app.js
+++ js/app.js
@@ -2,8 +2,11 @@ const app = {
     grid : [],
     row : [],
     currentPlayer: 'O',
+    winner: false,
     
     init: function() {
+        document.querySelector('body').innerHTML = "";
+        app.winner = false;
         app.grid = [
             ['','','','','','',''], 
             ['','','','','','',''], 
@@ -23,8 +26,8 @@ const app = {
         }
     },
 
-    displayLine : function(num_column) {
-        //console.log("num_column: " + num_column);
+    displayLine : function(rowIndex) {
+        //console.log("rowIndex: " + rowIndex);
 
         divRow = document.createElement("div");
         divRow.classList.add("row");
@@ -33,16 +36,16 @@ const app = {
         body.appendChild(divRow);
         
         let num = 0;
-        //for (currentCell of app.grid[num_column]) {
-        for (let num_line = 0; num_line < app.grid[num_column].length; num_line++) {
-            let currentCell = app.grid[num_column][num_line];
+        //for (currentCell of app.grid[rowIndex]) {
+        for (let num_column = 0; num_column < app.grid[rowIndex].length; num_column++) {
+            let currentCell = app.grid[rowIndex][num_column];
             divEl = document.createElement("div");
             divEl.classList.add("cell");
-            divEl.classList.add("position_"+num_column+"_"+num_line);
+            divEl.classList.add("position_"+rowIndex+"_"+num_column);
             divRow.appendChild(divEl);
             divEl.textContent = currentCell;
 
-            if (num_column == 0) {
+            if (rowIndex == 0) {
                 divEl.classList.add("column");
                 divEl.setAttribute('rel', num++);
             }
@@ -62,24 +65,33 @@ const app = {
         let num_column = this.getAttribute('rel');
         console.log("num_column: " + num_column);
         let num_line = 5;
-        //tant que la cellule sélectionné n'est pas vide et 
-        //que le numéro de la ligne est supérieur à zéro alors :
-        while (app.grid[num_line][num_column] != "" && num_line > 0) {
-            num_line--; //on décrémente de 1 le numéro de la ligne
-        }
-        //si la ligne est vide alors on rempli avec le jeton du joueur
-        if (app.grid[num_line][num_column] == "") {
-            app.grid[num_line][num_column] = app.currentPlayer;
-            document.querySelector(".position_" + num_line + "_" + num_column).textContent = app.currentPlayer;
-            //on regarde si on a un gagnant
-            let winner = app.detectWinner(num_line, num_column);
-            if (winner) {
-                alert(app.currentPlayer + " a gagné !");
+
+        if (app.winner == false) {
+            //tant que la cellule sélectionné n'est pas vide et 
+            //que le numéro de la ligne est supérieur à zéro alors :
+            while (app.grid[num_line][num_column] != "" && num_line > 0) {
+                num_line--; //on décrémente de 1 le numéro de la ligne
+            }
+            //si la ligne est vide alors on rempli avec le jeton du joueur
+            if (app.grid[num_line][num_column] == "") {
+                app.grid[num_line][num_column] = app.currentPlayer;
+                document.querySelector(".position_" + num_line + "_" + num_column).textContent = app.currentPlayer;
+                //on regarde si on a un gagnant
+                app.winner = app.detectWinner(num_line, num_column);
+                if (app.winner) {
+                    alert(app.currentPlayer + " a gagné !");
+                } else {
+                    app.switchPlayer();
+                }
             } else {
-                app.switchPlayer();
+                alert("Colonne entièrement remplie. Merci de choisir une autre colonne !");
             }
         } else {
-            alert("Colonne entièrement remplie. Merci de choisir une autre colonne !");
+            let reponse = confirm("La partie est déjà terminé. Souhaitez en relancer une nouvelle ?");
+
+            if (reponse) {
+                app.init();
+            }
         }
 
 
@@ -98,148 +110,139 @@ const app = {
         let valueCellCurrentPlayer = app.currentPlayer;
         let num_line_origin = num_line;
         let num_column_origin = num_column;
+        let data = { valueCellCurrentPlayer: valueCellCurrentPlayer, repeat: repeat, num_line: num_line, num_column: num_column, num_line_origin: num_line_origin, num_column_origin: num_column_origin};
         //tant que il n'y a pas 4 occurrences du jeton du joueur courant
         //ET que l'on n'a pas un jeton adverse à côté
         //ET qu'on a pas atteint le bord de la grille du puissance 4 alors :
 
         //sur la droite
         do {
-            valueCellCurrentPlayer = document.querySelector(".position_" + num_line + "_" + num_column).innerHTML;
-            if (valueCellCurrentPlayer == app.currentPlayer) {
-                repeat++;
-            }
-            console.log("position: " + num_line + "," + num_column + " valeur: " + valueCellCurrentPlayer);
-            console.log("repeat: " + repeat + " while 1");
-            num_column++;
-        } while (repeat < 4 && valueCellCurrentPlayer == app.currentPlayer && num_column < 7 );
+            data = app.checkDirection('right', data);
+        } while (data.repeat < 4 && data.valueCellCurrentPlayer == app.currentPlayer && data.num_column < 7 );
         
         //sur la gauche
-        num_line = num_line_origin;
-        num_column = num_column_origin;
-        console.log("num_line:" + num_line);
-        console.log("num_column:" + num_column);
-        if (repeat < 4) {
-            repeat--;
+        data = app.reinitLineColumn(data);
+        if (data.repeat < 4) {
+            data.repeat--;
         } else {
-            return (repeat >= 4);
+            return (data.repeat >= 4);
         }
         do {
-            valueCellCurrentPlayer = document.querySelector(".position_" + num_line + "_" + num_column).innerHTML;
-            if (valueCellCurrentPlayer == app.currentPlayer) {
-                repeat++;
-            }
-            console.log("position: " + num_line + "," + num_column + " valeur: " + valueCellCurrentPlayer);
-            console.log("repeat: " + repeat + " while 2");
-            num_column--;
-        } while (repeat < 4 && valueCellCurrentPlayer == app.currentPlayer && num_column >= 0 );
+            data = app.checkDirection('left', data);
+        } while (data.repeat < 4 && data.valueCellCurrentPlayer == app.currentPlayer && data.num_column >= 0 );
 
         
-        if (repeat < 4) {
-            repeat = 0;
+        if (data.repeat < 4) {
+            data.repeat = 0;
         } else {
-            return (repeat >= 4);
+            return (data.repeat >= 4);
         }
         //vers le bas
-        num_line = num_line_origin;
-        num_column = num_column_origin;
-        console.log("num_line:" + num_line);
-        console.log("num_column:" + num_column);
+        data = app.reinitLineColumn(data);
         do {
-            valueCellCurrentPlayer = document.querySelector(".position_" + num_line + "_" + num_column).innerHTML;
-            if (valueCellCurrentPlayer == app.currentPlayer) {
-                repeat++;
-            }
-            console.log("position: " + num_line + "," + num_column + " valeur: " + valueCellCurrentPlayer);
-            console.log("repeat: " + repeat + " while 3");
-            num_line++;
-        } while (repeat < 4 && valueCellCurrentPlayer == app.currentPlayer && num_line < 6 );
+            data = app.checkDirection('down', data);
+        } while (data.repeat < 4 && data.valueCellCurrentPlayer == app.currentPlayer && data.num_line < 6 );
 
-        if (repeat < 4) {
-            repeat = 0;
+        if (data.repeat < 4) {
+            data.repeat = 0;
         } else {
-            return (repeat >= 4);
+            return (data.repeat >= 4);
         }
         //diagonale haut gauche
-        num_line = num_line_origin;
-        num_column = num_column_origin;
-        console.log("num_line:" + num_line);
-        console.log("num_column:" + num_column);
+        data = app.reinitLineColumn(data);
         do {
-            valueCellCurrentPlayer = document.querySelector(".position_" + num_line + "_" + num_column).innerHTML;
-            if (valueCellCurrentPlayer == app.currentPlayer) {
-                repeat++;
-            }
-            console.log("position: " + num_line + "," + num_column + " valeur: " + valueCellCurrentPlayer);
-            console.log("repeat: " + repeat + " while 4");
-            num_line--;
-            num_column--;
-        } while (repeat < 4 && valueCellCurrentPlayer == app.currentPlayer && num_line >= 0 && num_column >= 0);
+            data = app.checkDirection('d_up_left', data);
+        } while (data.repeat < 4 && data.valueCellCurrentPlayer == app.currentPlayer && data.num_line >= 0 && data.num_column >= 0);
 
-        if (repeat < 4) {
-            repeat--;
+        if (data.repeat < 4) {
+            data.repeat--;
         } else {
-            return (repeat >= 4);
+            return (data.repeat >= 4);
         }
         //diagonale bas droite
-        num_line = num_line_origin;
-        num_column = num_column_origin;
-        console.log("num_line:" + num_line);
-        console.log("num_column:" + num_column);
+        data = app.reinitLineColumn(data);
         do {
-            valueCellCurrentPlayer = document.querySelector(".position_" + num_line + "_" + num_column).innerHTML;
-            if (valueCellCurrentPlayer == app.currentPlayer) {
-                repeat++;
-            }
-            console.log("position: " + num_line + "," + num_column + " valeur: " + valueCellCurrentPlayer);
-            console.log("repeat: " + repeat + " while 5");
-            num_line++;
-            num_column++;
-        } while (repeat < 4 && valueCellCurrentPlayer == app.currentPlayer && num_line < 6 && num_column < 7);
+            data = app.checkDirection('d_down_right', data);
+        } while (data.repeat < 4 && data.valueCellCurrentPlayer == app.currentPlayer && data.num_line < 6 && data.num_column < 7);
         
-        if (repeat < 4) {
-            repeat = 0;
+        if (data.repeat < 4) {
+            data.repeat = 0;
         } else {
-            return (repeat >= 4);
+            return (data.repeat >= 4);
         }
         //diagonale bas gauche
-        num_line = num_line_origin;
-        num_column = num_column_origin;
-        console.log("num_line:" + num_line);
-        console.log("num_column:" + num_column);
+        data = app.reinitLineColumn(data);
         do {
-            valueCellCurrentPlayer = document.querySelector(".position_" + num_line + "_" + num_column).innerHTML;
-            if (valueCellCurrentPlayer == app.currentPlayer) {
-                repeat++;
-            }
-            console.log("position: " + num_line + "," + num_column + " valeur: " + valueCellCurrentPlayer);
-            console.log("repeat: " + repeat + " while 6");
-            num_line++;
-            num_column--;
-        } while (repeat < 4 && valueCellCurrentPlayer == app.currentPlayer && num_line < 6 && num_column >= 0);
+            data = app.checkDirection('d_down_left', data);
+        } while (data.repeat < 4 && data.valueCellCurrentPlayer == app.currentPlayer && data.num_line < 6 && data.num_column >= 0);
 
-        if (repeat < 4) {
-            repeat--;
+        if (data.repeat < 4) {
+            data.repeat--;
         } else {
-            return (repeat >= 4);
+            return (data.repeat >= 4);
         }
         //diagonale haut droite
-        num_line = num_line_origin;
-        num_column = num_column_origin;
-        console.log("num_line:" + num_line);
-        console.log("num_column:" + num_column);
+        data = app.reinitLineColumn(data);
         do {
-            valueCellCurrentPlayer = document.querySelector(".position_" + num_line + "_" + num_column).innerHTML;
-            if (valueCellCurrentPlayer == app.currentPlayer) {
-                repeat++;
-            }
-            console.log("position: " + num_line + "," + num_column + " valeur: " + valueCellCurrentPlayer);
-            console.log("repeat: " + repeat + " while 7");
-            num_line--;
-            num_column++;
-        } while (repeat < 4 && valueCellCurrentPlayer == app.currentPlayer && num_line >= 0 && num_column < 7);
+            data = app.checkDirection('d_up_right', data);
+        } while (data.repeat < 4 && data.valueCellCurrentPlayer == app.currentPlayer && data.num_line >= 0 && data.num_column < 7);
 
-        return (repeat >= 4);
+        return (data.repeat >= 4);
+    },
+
+
+    checkDirection :  function(direction, data) {
+        data.valueCellCurrentPlayer = document.querySelector(".position_" + data.num_line + "_" + data.num_column).innerHTML;
+        if (data.valueCellCurrentPlayer == app.currentPlayer) {
+            data.repeat++;
+        }
+        console.log("position: " + data.num_line + "," + data.num_column + " valeur: " + data.valueCellCurrentPlayer);
+        console.log("repeat: " + data.repeat + " while 1");
+
+        switch(direction) {
+            case "right" :
+                data.num_column++;
+                break;
+            case "left" :
+                data.num_column--;
+                break;
+            case "down" :
+                data.num_line++;
+                break;
+            case "d_up_left" :
+                data.num_line--;
+                data.num_column--;
+                break;
+            case "d_down_right" :
+                data.num_line++;
+                data.num_column++;
+                break;
+            case "d_down_left" :
+                data.num_line++;
+                data.num_column--;
+                break;
+            case "d_up_right" :
+                data.num_line--;
+                data.num_column++;
+                break;
+        }
+
+        return data;
+    },
+
+
+    reinitLineColumn : function(data) {
+        data.num_line = data.num_line_origin;
+        data.num_column = data.num_column_origin;
+        console.log("num_line:" + data.num_line);
+        console.log("num_column:" + data.num_column);
+
+        return data;
     }
+
+
+
+
 
 };
 
